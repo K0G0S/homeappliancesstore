8d64a7512d0555eb8cfbb8237ce9dd5b8cde4396
gus fragos <konsfragos@gmail.com>
2021-01-22 13:29:14 +0200

    askisi 3,4,5

---
+++ src/homeappliancesstore.java
@@ -0,0 +1,45 @@
+import java.lang.String;
+import java.util.Scanner;
+
+public class homeappliancesstore {
+
+    public static String Namecomp, Idcomp;
+    public static int Stuffcomp;
+
+    public static void main(String[] args) {
+        try {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Grapse onoma etairias: ");
+            String Namecomp = myObj.nextLine();
+            if (Namecomp != " ") {
+                System.out.println("To onoma tis etairias einai:");
+                System.out.println(Namecomp);
+            }
+
+            {
+                Scanner myObj2 = new Scanner(System.in);
+                System.out.println("Grapse dieu8insi tis etairias: ");
+                String Idcomp = myObj2.nextLine();
+                if (Idcomp != " ") {
+                    System.out.println("To id tis etairias:");
+                    System.out.println(Idcomp);
+                }
+            }
+
+            {
+                Scanner myObj3 = new Scanner(System.in);
+                System.out.println("Posa atoma vouleuoun stin etairia: ");
+                int Stuffcomp = Integer.parseInt(myObj3.nextLine());
+                if (Stuffcomp > 0 ) {
+                    System.out.println("To prosopiko tis etairias einai:");
+                    System.out.println(Idcomp);
+                }
+            }
+        }
+        catch(Exception e) {
+            System.out.println(Namecomp);
+            System.out.println(Idcomp);
+            System.out.println(Stuffcomp);
+        }
+    }
+}
